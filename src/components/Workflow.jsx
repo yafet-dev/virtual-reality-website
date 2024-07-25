d1b47aaaf38973b3ce93b301ba0ab418ed1d94d2
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-07-25 07:03:29 +0300

    simple fix

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -6,9 +6,8 @@ function Workflow() {
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide ">
         Accelerate your{" "}
         <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
-          coding workflow
+          coding workflow.
         </span>
-        .
       </h2>
     </div>
   );
