16daa2df84b0d02ac3816241a52689ba0274549b
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-07-25 07:01:02 +0300

    add work flow section

---
+++ src/components/Workflow.jsx
@@ -0,0 +1,17 @@
+import { CheckCircle2 } from "lucide-react";
+import codeImg from "../assets/code.jpg";
+function Workflow() {
+  return (
+    <div className="mt-20">
+      <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide ">
+        Accelerate your{" "}
+        <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
+          coding workflow
+        </span>
+        .
+      </h2>
+    </div>
+  );
+}
+
+export default Workflow;
