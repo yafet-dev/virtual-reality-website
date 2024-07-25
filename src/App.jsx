0a140f80032c071f559349063813525921683618
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-07-25 07:01:28 +0300

    add workflow on the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import FeatureSection from "./components/FeatureSection";
 import HeroSection from "./components/HeroSection";
 import NavBar from "./components/NavBar";
+import Workflow from "./components/Workflow";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
         <FeatureSection />
+        <Workflow />
       </div>
     </>
   );
