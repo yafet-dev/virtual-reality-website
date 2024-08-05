e2e3ddc04394107ebc9e34d7b8fbe1ba707e5083
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-08-05 06:37:19 +0300

    update features

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -1,3 +1,4 @@
+import { features } from "../constants";
 function FeatureSection() {
   return (
     <div className=" mt-20 border-b border-neutral-800 min-h-[800px]">
@@ -6,6 +7,23 @@ function FeatureSection() {
           {" "}
           Features
         </span>
+
+        <h2 className="text-3xl sm:text-5xl lg:text-6xl lg:mt-20 tracking-wide">
+          Easily build{" "}
+          <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
+            your code
+          </span>
+        </h2>
+      </div>
+
+      <div className="flex flex-wrap mt-10 lg:mt-20">
+        {features.map((feature, index) => {
+          <div key={index} className="w-full sm:1/2 lg:w-1/3">
+            <div className="flex">
+              <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700"></div>
+            </div>
+          </div>;
+        })}
       </div>
     </div>
   );
