df5a31c29aeb43b9ad07c3f24e221e0a95098a12
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-09-02 08:11:39 +0300

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 function Footer() {
   return (
-    <footer>
-      <div></div>
+    <footer className="mt-20 border-5 py-10 border-neutral-700">
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+        <div></div>
+      </div>
     </footer>
   );
 }
