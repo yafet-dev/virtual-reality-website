98b2772f2f971585d692140f26b5ada7d12e8433
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-09-02 08:12:54 +0300

    add some styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,9 @@ function Footer() {
   return (
     <footer className="mt-20 border-5 py-10 border-neutral-700">
       <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
-        <div></div>
+        <div>
+          <h3 className="text-md font-semibold mb-4">Resources</h3>
+        </div>
       </div>
     </footer>
   );
