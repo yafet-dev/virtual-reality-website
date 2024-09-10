f78c20cd3785291c80a5087ecaf1290311acd045
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-09-10 08:14:48 +0300

    add resources links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
+import { resourcesLinks, platformLinks, communityLinks } from "../constants";
+
 function Footer() {
   return (
     <footer className="mt-20 border-5 py-10 border-neutral-700">
       <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
         <div>
           <h3 className="text-md font-semibold mb-4">Resources</h3>
+          <ul>{resourcesLinks}</ul>
         </div>
       </div>
     </footer>
