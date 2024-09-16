16cde5c254aac7c6ee8160fa35567621f3ba5e7e
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-09-16 12:36:24 +0300

    update indexjsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import HeroSection from "./components/HeroSection";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+      </div>
+    </>
+  );
+}
+
+export default App;
