c6bf32e7d67b9f186f84e82b4fc2be0a7aeb5039
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-09-17 02:39:43 +0300

    add feature section

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -1,5 +1,14 @@
 function FeatureSection() {
-  return <div>Feature section</div>;
+  return (
+    <div className=" mt-20 border-b border-neutral-800 min-h-[800px]">
+      <div className="text-center">
+        <span className="text-orange-500 rounded-full h-6 text-lg font-medium px-2 py-1 uppercase">
+          {" "}
+          Features
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default FeatureSection;
