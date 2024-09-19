6bc31fbd145cb99878298cebd1dbf14f39df1e24
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-09-19 06:59:15 +0300

    upload description

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -1,6 +1,6 @@
 import { CheckCircle2 } from "lucide-react";
 import codeImg from "../assets/code.jpg";
-import { checklistItems } from "../constants/index";
+import { checklistItems, navItems } from "../constants/index";
 function Workflow() {
   return (
     <div className="mt-20">
@@ -16,7 +16,17 @@ function Workflow() {
         </div>
         <div className="pt-12 w-full lg:w-1/2">
           {checklistItems.map((item, index) => {
-            return <div key={index}>{item.title}</div>;
+            return (
+              <div key={index} className="flex mb-12">
+                <div className="text-green-400 mx-6 bg-neutral-900 h-10 w-10 p-2 justify-center items-center rounded-full">
+                  <CheckCircle2 />
+                </div>
+                <div>
+                  <h5 className="mt-1 mb-2 text-xl">{item.title}</h5>
+                  <p className="text-neutral-500 text-md">{item.description}</p>
+                </div>
+              </div>
+            );
           })}
         </div>
       </div>
